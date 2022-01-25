5439157b231fe3fa9839204e1d186a2fa83c4cd4
Darek Mazur <dev@darekmazur.it>
2022-01-25 15:32:46 +0100

    crete global styles

---
+++ src/utils/themes.js
@@ -0,0 +1,18 @@
+export const theme = {
+  colors: {
+    white: '#fff',
+    green: '#076324',
+  },
+  fonts: {
+    mainFont: '"Montserrat", sans-serif',
+    headerFont: '"Sancreek", cursive;',
+  },
+  fontSize: {
+    xxl: '5rem',
+    xl: '4.2rem',
+    l: '3rem',
+    lm: '1.8rem',
+    m: '1.6rem',
+    s: '1.4rem',
+  },
+};
