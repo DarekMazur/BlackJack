ba0ae79857e669e051441ae09e8494f854046f6a
Darek Mazur <dev@darekmazur.it>
2022-01-25 20:03:28 +0100

    remove commented code

--- src/Components/organisms/Footer/FooterSocialMenu/FooterSocialMenu.js
+++ src/Components/organisms/Footer/FooterSocialMenu/FooterSocialMenu.js
@@ -1,24 +1,8 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
 import Menu from '../../../molecules/Menu/Menu';
-import { StyledMenu } from '../../../molecules/Menu/Menu.styles';
-import { StyledMenuItem } from '../../../molecules/Menu/MenuItem/MenuItem.styles';
 
 const FooterSocialMenu = () => {
-  return (
-    <Menu />
-    // <StyledMenu>
-    //   <StyledMenuItem>
-    //     <FontAwesomeIcon icon={['fab', 'twitter-square']} />
-    //   </StyledMenuItem>
-    //   <StyledMenuItem>
-    //     <FontAwesomeIcon icon={['fab', 'instagram']} />
-    //   </StyledMenuItem>
-    //   <StyledMenuItem>
-    //     <FontAwesomeIcon icon={['fab', 'github']} />
-    //   </StyledMenuItem>
-    // </StyledMenu>
-  );
+  return <Menu />;
 };
 
 export default FooterSocialMenu;
